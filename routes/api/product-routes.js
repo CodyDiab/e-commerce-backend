dc09a3d467df14ddb7098bcdd771ddb159ee359e
Diab <emailcodydiab@gmail.com>
2020-06-13 07:45:25 -0500

    created api endpoints for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,72 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    //  include its associated Category and Tag data
+    include: [
+      {
+        model:Category,
+        atrributes: ['category_name']
+      },
+      {
+        model:Tag,
+        attributes:['tag_name']
+      }
+    ]
+  })
+   .then(dbProduct => res.json(dbProduct))
+   .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   });
+  
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    // include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      
+      }
+    ]
+  })
+   .then(dbProduct => {
+     if(!dbProduct) {
+       res.status(404).json({message: 'There was no product found with this id'});
+       return;
+     }
+     res.json(dbProduct);
+   })
+   .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   });
 });
 
 // create new product
